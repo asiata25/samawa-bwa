8e89b2f7c4ec8c04ef80b137a309912aeec6d336
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-10-24 10:49:36 +0700

    design: navbar
    - add sort clasess plugin with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        xl: "75px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"Poppins"', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        brand: {
+          primary: "#FF48B6",
+          secondary: "#5C2D2D",
+        },
+      },
+      spacing: {
+        7.5: "30px",
+      },
+    },
   },
   plugins: [],
-}
+};
